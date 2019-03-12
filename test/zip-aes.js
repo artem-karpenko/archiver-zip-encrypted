6ee13f0a84a46da6c9d70276e5f9c88e3bca37a0
Artem Karpenko <a.karpenko@smissltd.com>
2019-03-12 09:25:38 +0200

    With aes-js

--- test/zip-aes.js
+++ test/zip-aes.js
@@ -9,19 +9,24 @@ const path = require('path');
  * This test assumes 7-Zip installed in the system and available in path
  */
 describe('zip-aes', () => {
+    before(() => {
+        try {
+            archiver.registerFormat('zip-encrypted', require("../"));
+        } catch (e) {} // already registered
+    });
+
     beforeEach(() => {
         rmrf.sync('./target');
         fs.mkdirSync('./target', {recursive: true});
     });
 
-    it('should pack zip-aes/unpack 7z', (done) => {
-        try {
-            archiver.registerFormat('zip-encrypted', require("../"));
-        } catch (e) {} // already registered
-
+    it('should pack with zip-aes and unpack with 7z', (done) => {
         let archive = archiver.create('zip-encrypted', {zlib: {level: 8}, encryptionMethod: 'aes256', password: '123'});
-        archive.append(fs.createReadStream('./test/resources/test.txt'), {
+        archive.append(fs.createReadStream('./test/resources/test.txt'), { // with stream
             name: 'test.txt'
+        });
+        archive.append(fs.readFileSync('./test/resources/test.txt'), { // with buffer
+            name: 'test2.txt'
         });
         archive.finalize();
 
@@ -32,8 +37,15 @@ describe('zip-aes', () => {
             cp.execFile('7z', ['e', `target${path.sep}test.zip`, '-otarget', '-p123'], (e) => {
                 should.not.exist(e, '7z throws error: ' + e);
                 fs.existsSync('./target/test.txt').should.be.true('Extracted file should exist');
+                fs.existsSync('./target/test2.txt').should.be.true('Extracted file should exist');
+
+                const originalFileContents = fs.readFileSync('./test/resources/test.txt').toString('utf-8');
+
                 fs.readFileSync('./target/test.txt').toString('utf-8').should.be.eql(
-                    fs.readFileSync('./test/resources/test.txt').toString('utf-8')
+                    originalFileContents
+                );
+                fs.readFileSync('./target/test2.txt').toString('utf-8').should.be.eql(
+                    originalFileContents
                 );
 
                 done();
