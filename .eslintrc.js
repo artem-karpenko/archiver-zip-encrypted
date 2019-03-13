d6b5777a45e497f910dc53f6b95abfda1aac5836
Artem Karpenko <a.karpenko@smissltd.com>
2019-03-14 00:22:56 +0200

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'env': {
+        'node': true,
+        'es6': true,
+        'mocha': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2015
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
